a28095cbbac1f333e96a658675062c2d05875cc8
shreyasnair02 <SHREYAS.20201ISE0085@presidencyuniversity.in>
2023-09-09 23:05:34 +0530

    config v1

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 
 app.use(express.json({ limit: "10mb" }));
 
